d589db32d2c9639d18b1d4d0fd1128345d9daf1c
wesliey muchiri <weslieymuchiri@gmail.com>
2022-09-08 14:51:57 -0700

    print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 
-
 /**
 * main - print the string in the put function
 *
@@ -9,11 +8,7 @@
 * this program prints "Programming is like building a multilingual puzzle
 * Return: 0
 */
-int main(void)
 {
-
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
-
 }
