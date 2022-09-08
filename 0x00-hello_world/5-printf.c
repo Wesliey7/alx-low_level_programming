d131b8147574691be4841f591ea07a5cffe11f0d
wesliey muchiri <weslieymuchiri@gmail.com>
2022-09-08 10:16:10 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 
 
@@ -21,7 +20,7 @@ int main(void)
 
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 
