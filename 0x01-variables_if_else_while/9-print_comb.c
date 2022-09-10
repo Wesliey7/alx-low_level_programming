e740168d0cac708fd7ea24565903ae5769676dab
wesliey muchiri <weslieymuchiri@gmail.com>
2022-09-10 04:10:44 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,14 +12,13 @@ int main(void)
 	for (c = 48; c <= 57; c++)
 	{
 		putchar(c);
-	if (c != 57)	
-
-
+		if (c != 57)
 		{
 			putchar(',');
 		putchar(' ');
 		}
-	}			
+
+	}
 	putchar('\n');
 	return (0);
 }
