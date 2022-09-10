c0ed88b205342a109938d5428692fba886221414
wesliey muchiri <weslieymuchiri@gmail.com>
2022-09-10 11:45:22 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,11 +8,9 @@ int main(void)
 	int c = 0;
 	int f_d;
 	int l_d;
-
 	while (c <= 99)
 	{
 		f_d = (c / 10 + '0');							l_d = (c % 10 + '0');
-
 		if (f_d < l_d)
 		{
 			putchar(f_d);
@@ -25,7 +23,8 @@ int main(void)
 		}
 		c++;
 	}
-	putchar('\n');
-	return (0);
+putchar('\n');
+return (0);
 }
+
 
