d952524dbb26a2e9c5093c84d70358df84f888b8
wesliey muchiri <weslieymuchiri@gmail.com>
2022-09-27 06:19:17 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * _strspn - gets length of a prefix substring
- * @s: string to be checked
+ * _strspn - gets lengthof a prefix substring
+ * @s: string to check
  * @accept: string to check against
  *
- * Return: number of bytes s in accept
+ * Return: number of bytes of s in accept
  */
-
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j;
@@ -17,13 +16,10 @@ unsigned int _strspn(char *s, char *accept)
 		for (j = 0; accept[j]; j++)
 		{
 			if (s[i] == accept[j])
-
-break;
+				break;
 		}
 		if (!accept[j])
-
-break;
+			break;
 	}
 	return (i);
 }
-
