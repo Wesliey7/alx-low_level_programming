bceddc60d3e7b957fcb6eef6267f7efe6f5196af
wesliey muchiri <weslieymuchiri@gmail.com>
2022-09-28 12:56:17 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+
+#include "main.h"
+/**
+ * _int _strlen_recursion
+ *  - returns the length of a string
+ *  @s: string to be used
+ *
+ *  Return: return lenght of the string
+ */
+int _strlen_recursion(char *s)
+{
+	int sum = 0;
+
+	if (*s != '\0')
+	{
+		sum++;
+		sum += _strlen_recursion(s + 1);
+	}
+	return (sum);
+}
+
+
